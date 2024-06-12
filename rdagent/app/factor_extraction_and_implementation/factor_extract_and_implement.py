b58ca69c916fa1aff52f18ba4d0894e257d5c42c
USTCKevinF <fengwenjun@mail.ustc.edu.cn>
2024-06-12 05:43:36 +0000

    add test function in factor_extract_and_implement.py

--- rdagent/app/factor_extraction_and_implementation/factor_extract_and_implement.py
+++ rdagent/app/factor_extraction_and_implementation/factor_extract_and_implement.py
@@ -12,6 +12,7 @@ from rdagent.document_process.document_analysis import (
 from rdagent.document_process.document_reader import load_and_process_pdfs_by_langchain
 from rdagent.factor_implementation.share_modules.factor_implementation_utils import load_data_from_dict
 from rdagent.factor_implementation.CoSTEER import CoSTEERFG
+import pickle
 from dotenv import load_dotenv
 
 
@@ -33,5 +34,15 @@ def extract_factors_and_implement(report_file_path: str):
 
     result = factor_generate_method.generate(factor_tasks)
 
+def test_implement():
+    factor_data = pickle.load(open("/home/finco/v-wenjunfeng/RD-Agent/factor_dict_original.pkl", "rb"))
+
+    factor_tasks = load_data_from_dict(factor_data)
+
+    factor_generate_method = CoSTEERFG()
+
+    result = factor_generate_method.generate(factor_tasks)
+
 if __name__ == "__main__":
-    extract_factors_and_implement("/home/finco/v-wenjunfeng/RD-Agent/report.pdf")
+    # extract_factors_and_implement("/home/finco/v-wenjunfeng/RD-Agent/report.pdf")
+    test_implement()
