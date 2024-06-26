727227be5b390df989512893c9ed727732d72a69
Young <afe.young@gmail.com>
2024-06-26 08:20:08 +0000

    Commit init framework

---
+++ rdagent/core/proposal/__init__.py
@@ -0,0 +1,37 @@
+"""
+
+"""
+
+
+
+
+
+class BeliefGen:
+    def gen(self, scenario_desc: str, ) -> Belief:
+        """
+        Motivation of the variable `scenario_desc`:
+        - Mocking a data-scientist is observing the scenario.
+
+        scenario_desc may conclude:
+        - data observation:
+        - Task information:
+        """
+        
+
+
+class Belief:
+    """
+    TODO: We may have better name for it.
+
+    Candidates:
+    - Hypothesis
+    """
+
+class BeliefSet:
+    """
+    # drop, append
+
+    belief_imp: list[float] | None  # importance of each belief
+    """
+    belief_l: list[Belief]
+
