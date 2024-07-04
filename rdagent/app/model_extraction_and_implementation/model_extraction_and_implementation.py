d50c9d97c8f251e08cfd97e085a3a74cd3f68143
xuyang1 <xuyang1@microsoft.com>
2024-07-04 10:11:29 +0000

    rename modelimplementation

--- rdagent/app/model_extraction_and_implementation/model_extraction_and_implementation.py
+++ rdagent/app/model_extraction_and_implementation/model_extraction_and_implementation.py
@@ -3,12 +3,12 @@ from dotenv import load_dotenv
 
 from rdagent.components.coder.model_coder.one_shot import ModelCodeWriter
 from rdagent.components.coder.model_coder.task_loader import (
-    ModelImplementationExperimentLoaderFromPDFfiles,
+    ModelExperimentLoaderFromPDFfiles,
 )
 
 
 def extract_models_and_implement(report_file_path: str = "../test_doc") -> None:
-    factor_tasks = ModelImplementationExperimentLoaderFromPDFfiles().load(report_file_path)
+    factor_tasks = ModelExperimentLoaderFromPDFfiles().load(report_file_path)
     implementation_result = ModelCodeWriter().generate(factor_tasks)
     return implementation_result
 
