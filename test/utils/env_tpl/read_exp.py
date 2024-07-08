4bc62d62c7b8b28e0c6e0b411e47d000b77e74c8
Linlang <Lv.Linlang@hotmail.com>
2024-07-08 14:35:56 +0800

    add code

--- test/utils/env_tpl/read_exp.py
+++ test/utils/env_tpl/read_exp.py
@@ -1,12 +1,10 @@
 import qlib
+from mlflow.tracking import MlflowClient
+from mlflow.entities import ViewType
 qlib.init()
 
 from qlib.workflow import R
 # here is the documents of the https://qlib.readthedocs.io/en/latest/component/recorder.html
-
-print(123)
-
-print(R.list_experiments())
 
 # TODO: list all the recorder and metrics 
 
@@ -14,13 +12,27 @@ print(R.list_experiments())
 experiments = R.list_experiments()
 
 # Iterate through each experiment to list its recorders and metrics
+experiment_name = None
 for experiment in experiments:
     print(f"Experiment: {experiment}")
-    recorders = R.list_recorders(experiment)
+    recorders = R.list_recorders(experiment_name=experiment)
+    # print(recorders)
     for recorder_id in recorders:
-        print(f"  Recorder ID: {recorder_id}")
-        recorder = R.get_recorder(recorder_id, experiment)
+        if recorder_id is not None:
+            experiment_name = experiment
+        print(f"Recorder ID: {recorder_id}")
+        recorder = R.get_recorder(recorder_id=recorder_id, experiment_name=experiment)
         metrics = recorder.list_metrics()
-        print(f"    Metrics: {metrics}")
+        print(f"Metrics: {metrics}")
 
 # TODO: get the latest recorder
+
+recorder_list = R.list_recorders(experiment_name="workflow")
+end_times = {key: value.info['end_time'] for key, value in recorder_list.items()}
+sorted_end_times = dict(sorted(end_times.items(), key=lambda item: item[1], reverse=True))
+latest_recorder_id = next(iter(sorted_end_times))
+print(f"Latest recorder ID: {latest_recorder_id}")
+latest_recorder = R.get_recorder(experiment_name=experiment_name, recorder_id=latest_recorder_id)
+print(f"Latest recorder: {latest_recorder}")
+metrics = latest_recorder.list_metrics()
+print(f"Latest Metrics: {metrics}")
