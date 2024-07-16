cdf6b3abbf8fc1cb31b78ff7f587f8b373be0eda
Xu Yang <peteryang@vip.qq.com>
2024-07-16 16:27:19 +0000

    fix app scripts

---
+++ rdagent/app/model_extraction_and_code/model_extraction_and_implementation.py
@@ -0,0 +1,21 @@
+# %%
+from dotenv import load_dotenv
+
+from rdagent.components.coder.model_coder.task_loader import (
+    ModelExperimentLoaderFromPDFfiles,
+)
+from rdagent.scenarios.qlib.experiment.model_experiment import QlibModelScenario
+from rdagent.scenarios.qlib.model_coder import QlibModelCoSTEER
+
+
+def extract_models_and_implement(report_file_path: str = "../test_doc") -> None:
+    scenario = QlibModelScenario()
+    exp = ModelExperimentLoaderFromPDFfiles().load(report_file_path)
+    exp = QlibModelCoSTEER(scenario).generate(exp)
+    return exp
+
+
+import fire
+
+if __name__ == "__main__":
+    fire.Fire(extract_models_and_implement)
