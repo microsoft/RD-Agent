702c830b8759cc6613618e9abbaed6de8d7ad9e6
Taozhi Wang <taozhi.mark.wang@gmail.com>
2024-07-25 07:10:51 +0000

    Reformat analysis.py

--- rdagent/app/quant_factor_benchmark/analysis.py
+++ rdagent/app/quant_factor_benchmark/analysis.py
@@ -1,169 +1,60 @@
-
 import json
-from pathlib import Path
 import pickle
 import pandas as pd
+from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-from rdagent.components.benchmark.eval_method import summarize_res as summarize_res
+from rdagent.components.benchmark.eval_method import FactorImplementEval
 from rdagent.components.benchmark.conf import BenchmarkSettings
 
+class BenchmarkAnalyzer:
+    def __init__(self, settings):
+        self.settings = settings
+        self.index_map = self.load_index_map()
 
-results = {
-    "1 round experiment": "log/2024-07-25_02-32-24-552766/1445941/2024-07-25_02-48-07-756153.pkl",
-}
+    def load_index_map(self):
+        index_map = {}
+        with open(self.settings.bench_data_path, "r") as file:
+            factor_dict = json.load(file)
+        for factor_name, data in factor_dict.items():
+            index_map[factor_name] = (factor_name, data["Category"], data["Difficulty"])
+        return index_map
 
-# Get index map from the json file
-index_map = {}
-bs = BenchmarkSettings()
-def load(json_file_path: Path) -> None:
-    with open(json_file_path, "r") as file:
-        factor_dict = json.load(file)
-    for factor_name, factor_data in factor_dict.items():
-        index_map[factor_name] = (factor_name, factor_data["Category"], factor_data["Difficulty"])
+    def load_data(self, file_path):
+        file_path = Path(file_path)
+        if not (file_path.is_file() and file_path.suffix == ".pkl"):
+            raise ValueError("Invalid file path")
+        
+        with file_path.open("rb") as f:
+            res = pickle.load(f)
+        
+        return res
 
-load(bs.bench_data_path)
-
-def load_data(file_path):
-    file_path = Path(file_path)
-
-    if not (file_path.is_file() and file_path.suffix == ".pkl" and file_path.name.startswith("res_")):
-        raise ValueError("You may get a invalid file path")
-
-    sum_df = []
-    with file_path.open("rb") as f:
-        res = pickle.load(f)
-
-    sum_df.append(summarize_res(res))
-    sum_df = pd.concat(sum_df, axis=1)
-    if sum_df.shape[0] == 0:
-        raise ValueError("No data in the file")
-    print(file_path, sum_df.shape)
-
-    index = [
-        "FactorSingleColumnEvaluator",
-        "FactorOutputFormatEvaluator",
-        "FactorRowCountEvaluator",
-        "FactorIndexEvaluator",
-        "FactorMissingValuesEvaluator",
-        "FactorEqualValueCountEvaluator",
-        "FactorCorrelationEvaluator",
-        "run factor error",
-    ]
-
-    # reindex in case of failing to run evaluator.
-    # If all implemented factors fail to run, some evaluators may not appear in the result.
-    sum_df = sum_df.reindex(index, axis=0)
-
-    sum_df_clean = sum_df.T.groupby(level=0).apply(lambda x: x.reset_index(drop=True))
-
-    # sum_df.columns
-
-    def get_run_error(sum_df_clean):
-        run_error = sum_df_clean["run factor error"]
-
-        run_error = run_error.unstack()
-
-        run_error = run_error.T.fillna(False).astype(bool)  # null indicate no exception
-
-        succ_rate = ~run_error
-        succ_rate = succ_rate.mean(axis=0).to_frame("success rate")
-        # make it display in a percentage rate
-        # succ_rate["success rate"] = succ_rate["success rate"].map(lambda x: f"{x:.2%}")
-        return succ_rate
-
-    succ_rate = get_run_error(sum_df_clean)
-
-    def reformat_succ_rate(display_df):
-        """You may get dataframe like this:
-
-                                    success rate
-        250-day_high_distance             80.00%
-        Corr_Close_Turnover               20.00%
-        EP_TTM                            20.00%
-        High_Frequency_Skewness           60.00%
-        Momentum                          50.00%
-        Morning_30-min_Return             30.00%
-        UID                                0.00%
-        Weighted_Earnings_Frequency       10.00%
-        """
-
+    def process_results(self, results):
+        final_res = {}
+        for experiment, path in results.items():
+            data = self.load_data(path)
+            summarized_data = FactorImplementEval.summarize_res(data)
+            processed_data = self.analyze_data(summarized_data)
+            final_res[experiment] = processed_data.iloc[-1, :]
+        return final_res
+    
+    def reformat_succ_rate(self, display_df):
         new_idx = []
-        display_df = display_df[display_df.index.isin(index_map.keys())]
-        # display_df = display_df.reindex(index_map.keys())
+        display_df = display_df[display_df.index.isin(self.index_map.keys())]
         for idx in display_df.index:
-            new_idx.append(index_map[idx])
+            new_idx.append(self.index_map[idx])
+
         display_df.index = pd.MultiIndex.from_tuples(
             new_idx,
             names=["Factor", "Category", "Difficulty"],
         )
-
         display_df = display_df.swaplevel(0, 2).swaplevel(0, 1).sort_index(axis=0)
 
-        def sort_key_func(x):
-            order_v = []
-            for i in x:
-                order_v.append({"Easy": 0, "Medium": 1, "Hard": 2, "New Discovery": 3}.get(i, i))
-            return order_v
-
-        return display_df.sort_index(key=sort_key_func)
-
-    succ_rate_f = reformat_succ_rate(succ_rate)
-    succ_rate_f
-
-    sum_df_clean["FactorRowCountEvaluator"]
-
-    def get_run_error(eval_series):
-        eval_series = eval_series.unstack()
-
-        succ_rate = eval_series.T.fillna(False).astype(bool)  # false indicate failure
-
-        succ_rate = succ_rate.mean(axis=0).to_frame("success rate")
-        # make it display in a percentage rate
-        # succ_rate["success rate"] = succ_rate["success rate"].map(lambda x: f"{x:.2%}")
-        return succ_rate
-
-    format_issue = (
-        sum_df_clean["FactorRowCountEvaluator"] & sum_df_clean["FactorIndexEvaluator"]
-    )
-
-    format_succ_rate = get_run_error(format_issue)
-    format_succ_rate_f = reformat_succ_rate(format_succ_rate)
-
-    corr = sum_df_clean["FactorCorrelationEvaluator"] * format_issue
-
-    corr = corr.unstack().T.mean(axis=0).to_frame("corr(only success)")
-    corr_res = reformat_succ_rate(corr)
-
-    corr_max = sum_df_clean["FactorCorrelationEvaluator"] * format_issue
-
-    corr_max = corr_max.unstack().T.max(axis=0).to_frame("corr(only success)")
-    corr_max_res = reformat_succ_rate(corr_max)
-
-    value_max = sum_df_clean["FactorMissingValuesEvaluator"] * format_issue
-    value_max = value_max.unstack().T.max(axis=0).to_frame("max_value")
-    value_max_res = reformat_succ_rate(value_max)
-
-    value_avg = (
-        (sum_df_clean["FactorMissingValuesEvaluator"] * format_issue)
-        .unstack()
-        .T.mean(axis=0)
-        .to_frame("avg_value")
-    )
-    value_avg_res = reformat_succ_rate(value_avg)
-
-    result_all = pd.concat(
-        {
-            "avg. Correlation (value only)": corr_res.iloc[:, 0],
-            "avg. Format successful rate": format_succ_rate_f.iloc[:, 0],
-            "avg. Run successful rate": succ_rate_f.iloc[:, 0],
-            "max. Correlation": corr_max_res.iloc[:, 0],
-            "max. accuracy": value_max_res.iloc[:, 0],
-            "avg. accuracy": value_avg_res.iloc[:, 0],
-        },
-        axis=1,
-    )
-
-    def result_all_key_order(x):
+        return display_df.sort_index(key=lambda x: [{"Easy": 0, "Medium": 1, "Hard": 2, "New Discovery": 3}.get(i, i) for i in x])
+    
+    def result_all_key_order(self, x):
         order_v = []
         for i in x:
             order_v.append(
@@ -178,93 +69,116 @@ def load_data(file_path):
             )
         return order_v
 
-    df = result_all.sort_index(axis=1, key=result_all_key_order)
-    print(df)
+    def analyze_data(self, sum_df):
+        index = [
+            "FactorSingleColumnEvaluator",
+            "FactorOutputFormatEvaluator",
+            "FactorRowCountEvaluator",
+            "FactorIndexEvaluator",
+            "FactorMissingValuesEvaluator",
+            "FactorEqualValueCountEvaluator",
+            "FactorCorrelationEvaluator",
+            "run factor error",
+        ]
+        sum_df = sum_df.reindex(index, axis=0)
+        sum_df_clean = sum_df.T.groupby(level=0).apply(lambda x: x.reset_index(drop=True))
 
-    # Calculate the mean of each column
-    mean_values = df.fillna(0.0).mean()
-    mean_df = pd.DataFrame(mean_values).T
+        run_error = sum_df_clean["run factor error"].unstack().T.fillna(False).astype(bool)
+        succ_rate = ~run_error
+        succ_rate = succ_rate.mean(axis=0).to_frame("success rate")
 
-    # TODO: set it as multi-index
-    # Assign the MultiIndex to the DataFrame
-    mean_df.index = pd.MultiIndex.from_tuples([("-", "-", "Average")], names=["Factor", "Category", "Difficulty"])
+        succ_rate_f = self.reformat_succ_rate(succ_rate)
+        succ_rate_f
 
-    # Append the mean values to the end of the dataframe
-    df_w_mean = pd.concat([df, mean_df]).astype("float")
+        sum_df_clean["FactorRowCountEvaluator"]
 
-    return df_w_mean
+        format_issue = (
+            sum_df_clean["FactorRowCountEvaluator"] & sum_df_clean["FactorIndexEvaluator"]
+        )
+        eval_series = format_issue.unstack()
+        succ_rate = eval_series.T.fillna(False).astype(bool)  # false indicate failure
+        format_succ_rate = succ_rate.mean(axis=0).to_frame("success rate")
+        format_succ_rate_f = self.reformat_succ_rate(format_succ_rate)
 
+        corr = sum_df_clean["FactorCorrelationEvaluator"] * format_issue
+        corr = corr.unstack().T.mean(axis=0).to_frame("corr(only success)")
+        corr_res = self.reformat_succ_rate(corr)
+        corr_max = sum_df_clean["FactorCorrelationEvaluator"] * format_issue
 
-def display_df(df):
-    # This depends on jupyter
-    def _single_formatter(column):
-        if column.endswith("rate"):
+        corr_max = corr_max.unstack().T.max(axis=0).to_frame("corr(only success)")
+        corr_max_res = self.reformat_succ_rate(corr_max)
 
-            def _f(x):
-                return "{:.2%}".format(x) if pd.notnull(x) else "-"
+        value_max = sum_df_clean["FactorMissingValuesEvaluator"] * format_issue
+        value_max = value_max.unstack().T.max(axis=0).to_frame("max_value")
+        value_max_res = self.reformat_succ_rate(value_max)
 
-        else:
+        value_avg = (
+            (sum_df_clean["FactorMissingValuesEvaluator"] * format_issue)
+            .unstack()
+            .T.mean(axis=0)
+            .to_frame("avg_value")
+        )
+        value_avg_res = self.reformat_succ_rate(value_avg)
 
-            def _f(x):
-                return "{:.4}".format(x) if pd.notnull(x) else "-"
+        result_all = pd.concat(
+            {
+                "avg. Correlation (value only)": corr_res.iloc[:, 0],
+                "avg. Format successful rate": format_succ_rate_f.iloc[:, 0],
+                "avg. Run successful rate": succ_rate_f.iloc[:, 0],
+                "max. Correlation": corr_max_res.iloc[:, 0],
+                "max. accuracy": value_max_res.iloc[:, 0],
+                "avg. accuracy": value_avg_res.iloc[:, 0],
+            },
+            axis=1,
+        )
 
-        return _f
+        df = result_all.sort_index(axis=1, key=self.result_all_key_order)
+        print(df)
 
-    def get_formatters():
-        # Show NaN or None as '-'.  Don't convert the value to string
-        fmts = {column: _single_formatter(column) for column in df.columns}
-        return fmts
+        # Calculate the mean of each column
+        mean_values = df.fillna(0.0).mean()
+        mean_df = pd.DataFrame(mean_values).T
 
-    # TODO display
-    df_w_mean.drop(["max. accuracy", "avg. accuracy"], axis=1).style.format(get_formatters()).background_gradient(
-        axis=0, vmax=1, vmin=0, cmap=__import__("seaborn").light_palette("green", as_cmap=True)
-    )
+        # Assign the MultiIndex to the DataFrame
+        mean_df.index = pd.MultiIndex.from_tuples([("-", "-", "Average")], names=["Factor", "Category", "Difficulty"])
 
+        # Append the mean values to the end of the dataframe
+        df_w_mean = pd.concat([df, mean_df]).astype("float")
 
-final_res = {}
-for k, p in results.items():
-    df = load_data(p)
-    print(df)
-    final_res[k] = df.iloc[-1, :]
-
-final_res = pd.DataFrame(final_res)
-
-
-# TODO plot it with seaborn and save it as a file
-final_res.drop(["max. accuracy", "avg. accuracy"], axis=0).T
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-plt.rcParams["axes.unicode_minus"] = False
-
-
-def change_fs(font_size):
-    font_size = font_size
-    plt.rc("font", size=font_size)  # controls default text sizes
-    plt.rc("axes", titlesize=font_size)  # fontsize of the axes title
-    plt.rc("axes", labelsize=font_size)  # fontsize of the x and y labels
-    plt.rc("xtick", labelsize=font_size)  # fontsize of the tick labels
-    plt.rc("ytick", labelsize=font_size)  # fontsize of the tick labels
-    plt.rc("legend", fontsize=font_size)  # legend fontsize
-    plt.rc("figure", titlesize=font_size)  # fontsize of the figure title
+        return df_w_mean
 
 
-change_fs(20)
 
+class Plotter:
+    @staticmethod
+    def change_fs(font_size):
+        plt.rc("font", size=font_size)
+        plt.rc("axes", titlesize=font_size)
+        plt.rc("axes", labelsize=font_size)
+        plt.rc("xtick", labelsize=font_size)
+        plt.rc("ytick", labelsize=font_size)
+        plt.rc("legend", fontsize=font_size)
+        plt.rc("figure", titlesize=font_size)
 
-# Prepare the data for plotting
-plot_data = final_res.drop(["max. accuracy", "avg. accuracy"], axis=0).T
-plot_data = plot_data.reset_index().melt("index", var_name="a", value_name="b")
+    @staticmethod
+    def plot_data(data, file_name):
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x="index", y="b", hue="a", data=data)
+        plt.xlabel("Method")
+        plt.ylabel("Value")
+        plt.title("Comparison of Different Methods")
+        plt.savefig(file_name)
 
-# Create the plot
-plt.figure(figsize=(10, 6))
-sns.barplot(x="index", y="b", hue="a", data=plot_data)
+if __name__ == "__main__":
+    settings = BenchmarkSettings()
+    benchmark = BenchmarkAnalyzer(settings)
+    results = {
+        "1 round experiment": "git_ignore_folder/eval_results/res_promptV220240724-060037.pkl",
+    }
+    final_results = benchmark.process_results(results)
+    final_results_df = pd.DataFrame(final_results)
 
-# Set the labels and title
-plt.xlabel("Method")
-plt.ylabel("Value")
-plt.title("Comparison of Different Methods")
-
-# Save the plot as a file
-plt.savefig("comparison_plot.png")
+    Plotter.change_fs(20)
+    plot_data = final_results_df.drop(["max. accuracy", "avg. accuracy"], axis=0).T
+    plot_data = plot_data.reset_index().melt("index", var_name="a", value_name="b")
+    Plotter.plot_data(plot_data, "rdagent/app/quant_factor_benchmark/comparison_plot.png")
