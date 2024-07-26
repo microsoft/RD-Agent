6b4b4fc91ddca9c921de30fffe71c50299c490c3
you-n-g <you-n-g@users.noreply.github.com>
2024-07-26 14:29:19 +0800

    Support special step customization (#123)
    * Support special step customization
    
    * lint

--- rdagent/app/qlib_rd_loop/factor_w_sc.py
+++ rdagent/app/qlib_rd_loop/factor_w_sc.py
@@ -2,15 +2,27 @@
 Factor workflow with session control
 """
 
+from typing import Any
+
 import fire
 
 from rdagent.app.qlib_rd_loop.conf import FACTOR_PROP_SETTING
 from rdagent.components.workflow.rd_loop import RDLoop
 from rdagent.core.exception import FactorEmptyError
+from rdagent.log import rdagent_logger as logger
 
 
 class FactorRDLoop(RDLoop):
     skip_loop_error = (FactorEmptyError,)
+
+    def exp_gen(self, prev_out: dict[str, Any]):
+        with logger.tag("r"):  # research
+            exp = self.hypothesis2experiment.convert(prev_out["propose"], self.trace)
+            if exp is None:
+                logger.error(f"Factor extraction failed.")
+                raise FactorEmptyError("Factor extraction failed.")
+            logger.log_object(exp.sub_tasks, tag="experiment generation")
+        return exp
 
 
 def main(path=None, step_n=None):
