234109392c6a5fa5aa120bc7966914d2264fca64
Xisen-Wang <xisen_application@163.com>
2024-07-31 07:06:16 +0000

    2-Line-3-Version Knowledge

--- rdagent/log/ui/app.py
+++ rdagent/log/ui/app.py
@@ -12,7 +12,8 @@ from rdagent.log.ui.web import (
 )
 
 # show logs folder
-WebView(TraceWindow()).display(FileStorage("/data/home/bowen/workspace/RD-Agent/log/yuante/2024-07-24_04-03-33-691119"))
+# WebView(TraceWindow()).display(FileStorage("/home/v-xisenwang/RD-Agent/log/2024-07-30_03-58-06-550431"))
+WebView(TraceWindow()).display(FileStorage("/data/userdata/v-xisenwang/RD-Agent/log/2024-07-30_09-54-33-167179/"))
 # WebView(TraceWindow()).display(FileStorage("/data/home/bowen/workspace/RD-Agent/log/2024-07-22_03-01-12-021659"))
 # WebView(TraceWindow()).display(FileStorage("./log/2024-07-18_08-37-00-477228"))
 
