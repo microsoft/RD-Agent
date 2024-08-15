438f4269fc93e3d3b59c487f402a4c52f8d8740b
Bowen Xian <xianbowen@outlook.com>
2024-08-15 07:06:20 +0000

    change kaggle api

--- rdagent/app/kaggle/start.py
+++ rdagent/app/kaggle/start.py
@@ -1,17 +1,12 @@
 from rdagent.utils.env import DockerEnv, KaggleConf
-
-from kaggle.api.kaggle_api_extended import KaggleApi
+import subprocess
 import zipfile
-
-
-api = KaggleApi()
-api.authenticate()
-
 competition_name = "playground-series-s4e8"
 
 # download data
 local_data_path = f"/data/userdata/share/kaggle/{competition_name}"
-api.competition_download_files(competition_name, path=local_data_path)
+subprocess.run(["kaggle", "competitions", "download", "-c", competition_name, "-p", local_data_path])
+
 # unzip data
 with zipfile.ZipFile(f"{local_data_path}/{competition_name}.zip", 'r') as zip_ref:
     zip_ref.extractall(local_data_path)
@@ -23,4 +18,4 @@ kaggle_conf.extra_volumes = {local_data_path: f"/kaggle/input/{competition_name}
 kaggle_env = DockerEnv(kaggle_conf)
 
 # use kaggle container to run the code
-logs = kaggle_env.run(entry="ls /kaggle/input")
+logs = kaggle_env.run(entry="python -c 'print(\"hello\")'")
