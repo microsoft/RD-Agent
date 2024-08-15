abee052ff2476ac03c01366001cb84f9069e59ca
Bowen Xian <xianbowen@outlook.com>
2024-08-15 08:13:47 +0000

    change structure

--- rdagent/scenarios/kaggle/experiment/workspace.py
+++ rdagent/scenarios/kaggle/experiment/workspace.py
@@ -1,4 +1,44 @@
 from pathlib import Path
+import pandas as pd
+
+from rdagent.app.data_mining.conf import PROP_SETTING
+from rdagent.core.experiment import FBWorkspace
+from rdagent.log import rdagent_logger as logger
+from rdagent.utils.env import KGDockerEnv, DockerEnv, KaggleConf
+
+import subprocess
+import zipfile
+
+class KaggleFBWorkspace(FBWorkspace):
+    def __init__(self,
+                 competition: str,
+                 shared_data_path: str = "/data/userdata/share/kaggle",
+                 *args,
+                 **kwargs) -> None:
+
+        super().__init__(*args, **kwargs)
+        self.competition = competition
+
+        # download data
+        local_data_path = f"{shared_data_path}/{competition}"
+        subprocess.run(["kaggle", "competitions", "download", "-c", competition, "-p", local_data_path])
+
+        # unzip data
+        with zipfile.ZipFile(f"{local_data_path}/{competition}.zip", 'r') as zip_ref:
+            zip_ref.extractall(local_data_path)
+        
+        self.kaggle_conf = KaggleConf()
+        # mount kaggle input data
+        self.kaggle_conf.extra_volumes = {local_data_path: f"/kaggle/input/{competition}"}
+
+    def execute(self, *args, **kwargs) -> str:
+        # TODO: run codes of the kaggle competition
+        kaggle_env = DockerEnv(self.kaggle_conf)
+        kaggle_env.prepare()
+
+        kaggle_env.run(entry=f"ls /kaggle/input/{self.competition}")
+
+from pathlib import Path
 
 import pandas as pd
 
