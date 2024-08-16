852243342b8766a6e9d0c3961770226ea51facc9
WinstonLiye <1957922024@qq.com>
2024-08-16 07:05:02 +0000

    refeact the code

--- rdagent/scenarios/kaggle/experiment/workspace.py
+++ rdagent/scenarios/kaggle/experiment/workspace.py
@@ -9,6 +9,28 @@ from rdagent.utils.env import KGDockerEnv, DockerEnv, KaggleConf
 import subprocess
 import zipfile
 
+class KGFBWorkspace(FBWorkspace):
+    def __init__(self, template_folder_path: Path, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.inject_code_from_folder(template_folder_path)
+
+    def execute(self, run_env: dict = {}, *args, **kwargs) -> str:
+        qtde = KGDockerEnv()
+        qtde.prepare()
+
+        execute_log = qtde.run(
+            local_path=str(self.workspace_path),
+            entry=f"python train.py",
+            env=run_env,
+        )
+
+        csv_path = self.workspace_path / "submission.csv"
+
+        if not csv_path.exists():
+            logger.error(f"File {csv_path} does not exist.")
+            return None
+        return pd.read_csv(csv_path, index_col=0).iloc[:, 0]
+
 class KaggleFBWorkspace(FBWorkspace):
     def __init__(self,
                  competition: str,
