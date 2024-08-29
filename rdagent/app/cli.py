0749b0defb472e279c23f52234a3ffb732f8c03e
SunsetWolf <Lv.Linlang@hotmail.com>
2024-08-29 10:03:34 +0000

    fix black error

--- rdagent/app/cli.py
+++ rdagent/app/cli.py
@@ -45,15 +45,17 @@ def ui(port=80, log_dir="", debug=False):
             cmds.append("--debug")
         subprocess.run(cmds)
 
+
 def find_project_root():
     """Find the root directory of the project."""
     current_path = Path.cwd()
-    marker = 'pyproject.toml'
+    marker = "pyproject.toml"
     current_path = current_path.resolve()
     for parent in [current_path] + list(current_path.parents):
         if (parent / marker).exists():
             return parent
     return None
+
 
 def sys_info():
     """collect system related info"""
@@ -67,23 +69,26 @@ def sys_info():
         logger.info(f"{method[0]}{getattr(platform, method[1])()}")
     return None
 
+
 def python_info():
     """collect Python related info"""
     python_version = sys.version.replace("\n", " ")
     logger.info(f"Python version: {python_version}")
     return None
 
+
 def rdagent_info():
     """collect rdagent related info"""
     root_dir = find_project_root()
-    current_version = get_version(root = root_dir)
+    current_version = get_version(root=root_dir)
     logger.info(f"RD-Agent version: {current_version.split('+')[0]}")
     package_list = [line for file in root_dir.joinpath("requirements").rglob("*") for line in open(file)]
-    package_name_list = [item.strip() for item in package_list if not item.startswith('#')]
+    package_name_list = [item.strip() for item in package_list if not item.startswith("#")]
     for package in package_name_list:
         version = pkg_resources.get_distribution(package).version
         logger.info(f"{package} version: {version}")
     return None
+
 
 def collect_info():
     """Prints information about the system and the installed packages."""
