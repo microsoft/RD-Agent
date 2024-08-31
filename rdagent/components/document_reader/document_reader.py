28a1650e0e78bfbb00f03c549c6037612d73b50a
you-n-g <you-n-g@users.noreply.github.com>
2024-08-31 20:35:04 +0800

    fix: package dependency. (#234)
    * fix package
    
    * fix lint

--- rdagent/components/document_reader/document_reader.py
+++ rdagent/components/document_reader/document_reader.py
@@ -8,11 +8,7 @@ import fitz
 import requests
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
-from langchain.document_loaders import (
-    OnlinePDFLoader,
-    PyPDFDirectoryLoader,
-    PyPDFLoader,
-)
+from langchain_community.document_loaders import PyPDFDirectoryLoader, PyPDFLoader
 from PIL import Image
 
 if TYPE_CHECKING:
