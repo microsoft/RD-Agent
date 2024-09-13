e09c8a076a596c4f9532810300fa4d1a64ccd8d1
WinstonLiye <1957922024@qq.com>
2024-09-13 09:26:59 +0000

    add the process of extracting exp from docs

--- rdagent/scenarios/kaggle/knowledge_management/vector_base.py
+++ rdagent/scenarios/kaggle/knowledge_management/vector_base.py
@@ -214,7 +214,7 @@ class KaggleExperienceBase(PDVectorBase):
 
 if __name__ == "__main__":
 
-    kaggle_base = KaggleExperienceBase(kaggle_experience_path="rdagent/scenarios/kaggle/knowledge_management/kaggle_experience.json")
+    kaggle_base = KaggleExperienceBase(kaggle_experience_path="git_ignore_folder/experience/tabular_cases/kaggle_experience_results.json")
     
     kaggle_base.add_experience_to_vector_base()
 
