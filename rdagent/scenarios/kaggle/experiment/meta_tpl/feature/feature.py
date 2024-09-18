d74da1fd56550652c27cc005f4fd1c93631b59e7
Xu Yang <peteryang@vip.qq.com>
2024-09-18 11:20:12 +0000

    update new feature engineering code format

--- rdagent/scenarios/kaggle/experiment/meta_tpl/feature/feature.py
+++ rdagent/scenarios/kaggle/experiment/meta_tpl/feature/feature.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
 """
-Here is the feature engineering code for each task, with the function name specified as feat_eng. 
-The file name should start with feat_, followed by the specific task name.
+Here is the feature engineering code for each task, with a class that has a fit and transform method.
+Remember
 """
 
+class IdentityFeature:
+    def fit(self, train_df: pd.DataFrame):
+        """
+        Fit the feature engineering model to the training data.
+        """
+        pass
 
-def feat_eng(X: pd.DataFrame):
-    """
-    return the selected features
-    """
-    return X
+    def transform(self, X: pd.DataFrame):
+        """
+        Transform the input data.
+        """
+        return X
+
+feature_engineering_cls = IdentityFeature
