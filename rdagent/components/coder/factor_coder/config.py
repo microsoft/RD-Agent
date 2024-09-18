9ad36da7557fc432799ad84cd464ee6d1af65981
Taozhi Wang <taozhi.mark.wang@gmail.com>
2024-09-18 05:55:51 +0000

    Search enhancement

--- rdagent/components/coder/factor_coder/config.py
+++ rdagent/components/coder/factor_coder/config.py
@@ -55,6 +55,9 @@ class FactorImplementSettings(BaseSettings):
     knowledge_base_path: Union[str, None] = None
     """Path to the knowledge base"""
 
+    data_tables_knowledge_path: Union[str, None] = None
+    """Path to the data tables knowledge"""
+
     new_knowledge_base_path: Union[str, None] = None
     """Path to the new knowledge base"""
 
