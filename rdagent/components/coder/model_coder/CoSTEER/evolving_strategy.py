dcb427a0785308ca99b39ff97454d45ad9c68c40
WinstonLiye <1957922024@qq.com>
2024-09-20 07:47:31 +0000

    fix a ci bug

--- rdagent/components/coder/model_coder/CoSTEER/evolving_strategy.py
+++ rdagent/components/coder/model_coder/CoSTEER/evolving_strategy.py
@@ -11,7 +11,11 @@ from rdagent.components.coder.model_coder.CoSTEER.evolvable_subjects import (
 from rdagent.components.coder.model_coder.CoSTEER.knowledge_management import (
     ModelQueriedKnowledge,
 )
-from rdagent.components.coder.model_coder.model import ModelExperiment, ModelFBWorkspace, ModelTask
+from rdagent.components.coder.model_coder.model import (
+    ModelExperiment,
+    ModelFBWorkspace,
+    ModelTask,
+)
 from rdagent.core.conf import RD_AGENT_SETTINGS
 from rdagent.core.evolving_framework import EvolvingStrategy
 from rdagent.core.prompts import Prompts
