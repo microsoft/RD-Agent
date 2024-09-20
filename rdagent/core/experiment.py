6c47d545d92914853551e3570f4c7e58c4c95dc2
Xu Yang <peteryang@vip.qq.com>
2024-09-20 03:35:25 +0000

    fix CI

--- rdagent/core/experiment.py
+++ rdagent/core/experiment.py
@@ -113,7 +113,7 @@ class FBWorkspace(Workspace):
         self.workspace_path.mkdir(parents=True, exist_ok=True)
 
     @staticmethod
-    def link_data_to_workspace(data_path: Path, workspace_path: Path):
+    def link_data_to_workspace(data_path: Path, workspace_path: Path) -> None:
         data_path = Path(data_path).absolute()  # in case of relative path that will be invalid when we change cwd.
         workspace_path = Path(workspace_path)
         for data_file_path in data_path.iterdir():
