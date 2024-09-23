954c32890a0f077d720471f793780c5f33ba3c05
Bowen Xian <xianbowen@outlook.com>
2024-09-23 06:13:16 +0000

    fix kaggle data founding in feature running

--- rdagent/scenarios/kaggle/experiment/workspace.py
+++ rdagent/scenarios/kaggle/experiment/workspace.py
@@ -48,6 +48,11 @@ class KGFBWorkspace(FBWorkspace):
                 "X_test.pkl",
                 "passenger_ids.pkl",
             ],
+            running_extra_volume=(
+                {KAGGLE_IMPLEMENT_SETTING.local_data_path + "/" + KAGGLE_IMPLEMENT_SETTING.competition: "/kaggle/input"}
+                if KAGGLE_IMPLEMENT_SETTING.competition
+                else None
+            ),
         )
         if results is None:
             logger.error("Feature preprocess failed.")
