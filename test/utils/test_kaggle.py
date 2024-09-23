91a619618be1d7db660ea2b413a78dfaba9417a1
XianBW <36835909+XianBW@users.noreply.github.com>
2024-09-23 13:33:46 +0800

    fix: test kaggle method (#296)
    * fix test kaggle
    
    * add hint for kaggle template test

--- test/utils/test_kaggle.py
+++ test/utils/test_kaggle.py
@@ -13,18 +13,21 @@ from rdagent.utils.agent.tpl import T
 
 class TestTpl(unittest.TestCase):
     def test_competition_template(self):
+        """
+        export KG_COMPETITION=<competition_name> before running this test
+        """
         competition = KAGGLE_IMPLEMENT_SETTING.competition
         print(f"[bold orange]{competition}[/bold orange]")
         ws = KGFBWorkspace(
             template_folder_path=Path(__file__).parent.parent.parent
             / "rdagent/scenarios/kaggle/experiment"
-            / f"{KAGGLE_IMPLEMENT_SETTING.competition}_template"
+            / f"{competition}_template"
         )
         print(ws.workspace_path)
         ws.execute()
         success = (ws.workspace_path / "submission.csv").exists()
+        self.assertTrue(success, "submission.csv is not generated")
         ws.clear()
-        return success
 
 
 if __name__ == "__main__":
