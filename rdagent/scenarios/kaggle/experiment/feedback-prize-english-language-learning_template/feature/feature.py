0c029e9572ee0e0c9f90297d60cac14dab31b15f
Bowen Xian <xianbowen@outlook.com>
2024-09-25 08:01:01 +0000

    change feedback tpl

--- rdagent/scenarios/kaggle/experiment/feedback-prize-english-language-learning_template/feature/feature.py
+++ rdagent/scenarios/kaggle/experiment/feedback-prize-english-language-learning_template/feature/feature.py
@@ -1,16 +1,23 @@
-import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+
+"""
+Here is the feature engineering code for each task, with a class that has a fit and transform method.
+Remember
+"""
 
 
-class TfidfFeature:
+class IdentityFeature:
     def fit(self, train_df: pd.DataFrame):
-        train_df = np.array(train_df).tolist()
-        train_X = list(map("".join, train_df))
-        self.model = TfidfVectorizer(stop_words="english", max_df=0.5, min_df=0.01).fit(train_X)
-        # print(self.model.get_feature_names_out()[:5])
+        """
+        Fit the feature engineering model to the training data.
+        """
+        pass
 
     def transform(self, X: pd.DataFrame):
-        X = np.array(X).tolist()
-        X = list(map("".join, X))
-        return self.model.transform(X)
+        """
+        Transform the input data.
+        """
+        return X
+
+
+feature_engineering_cls = IdentityFeature
