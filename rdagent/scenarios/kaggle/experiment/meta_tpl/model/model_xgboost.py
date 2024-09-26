2a1799a38aefd43b0e2b1213be08ec77b04ebe97
Xisen-Wang <xisen_application@163.com>
2024-09-26 13:00:38 +0000

    Adding the competition: Optiver Volatility Prediction

--- rdagent/scenarios/kaggle/experiment/meta_tpl/model/model_xgboost.py
+++ rdagent/scenarios/kaggle/experiment/meta_tpl/model/model_xgboost.py
@@ -4,6 +4,8 @@ motivation  of the model
 
 import pandas as pd
 import xgboost as xgb
+import numpy as np
+from sklearn.metrics import mean_squared_error
 
 
 def select(X: pd.DataFrame) -> pd.DataFrame:
@@ -12,21 +14,31 @@ def select(X: pd.DataFrame) -> pd.DataFrame:
 
 
 def fit(X_train: pd.DataFrame, y_train: pd.DataFrame, X_valid: pd.DataFrame, y_valid: pd.DataFrame):
-    """Define and train the model. Merge feature_select"""
     X_train = select(X_train)
     X_valid = select(X_valid)
+    
+    params = {
+        'objective': 'reg:squarederror',
+        'eval_metric': 'rmse',
+        'nthread': -1,
+        'eta': 0.1,
+        'max_depth': 6,
+        'subsample': 0.8,
+        'colsample_bytree': 0.8
+    }
+    
     dtrain = xgb.DMatrix(X_train, label=y_train)
     dvalid = xgb.DMatrix(X_valid, label=y_valid)
-
-    # TODO: for quick running....
-    params = {
-        "nthred": -1,
-    }
+    
     num_round = 100
-
-    evallist = [(dtrain, "train"), (dvalid, "eval")]
-    bst = xgb.train(params, dtrain, num_round, evallist)
-
+    
+    evallist = [(dtrain, 'train'), (dvalid, 'eval')]
+    bst = xgb.train(params, dtrain, num_round, evallist, early_stopping_rounds=50, verbose_eval=100)
+    
+    y_valid_pred = bst.predict(dvalid)
+    rmse = np.sqrt(mean_squared_error(y_valid, y_valid_pred))
+    print(f"Validation RMSE: {rmse:.4f}")
+    
     return bst
 
 
