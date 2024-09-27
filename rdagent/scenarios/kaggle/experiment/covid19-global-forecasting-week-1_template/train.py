e3394a2b856e20d0ac50e23cafe6e3867b279cd3
WinstonLiye <1957922024@qq.com>
2024-09-27 20:28:57 +0000

    change the template

--- rdagent/scenarios/kaggle/experiment/covid19-global-forecasting-week-1_template/train.py
+++ rdagent/scenarios/kaggle/experiment/covid19-global-forecasting-week-1_template/train.py
@@ -29,34 +29,63 @@ def import_module_from_path(module_name, module_path):
 # 1) Preprocess the data
 X_train, X_valid, y_train, y_valid, X_test, forecast_ids = preprocess_script()
 
-# 2) Train the model
+# 2) Auto feature engineering
+X_train_l, X_valid_l = [], []
+X_test_l = []
+
+for f in DIRNAME.glob("feature/feat*.py"):
+    cls = import_module_from_path(f.stem, f).feature_engineering_cls()
+    cls.fit(X_train)
+    X_train_f = cls.transform(X_train)
+    X_valid_f = cls.transform(X_valid)
+    X_test_f = cls.transform(X_test)
+
+    if X_train_f.shape[-1] == X_valid_f.shape[-1] and X_train_f.shape[-1] == X_test_f.shape[-1]:
+        X_train_l.append(X_train_f)
+        X_valid_l.append(X_valid_f)
+        X_test_l.append(X_test_f)
+
+X_train = pd.concat(X_train_l, axis=1, keys=[f"feature_{i}" for i in range(len(X_train_l))])
+X_valid = pd.concat(X_valid_l, axis=1, keys=[f"feature_{i}" for i in range(len(X_valid_l))])
+X_test = pd.concat(X_test_l, axis=1, keys=[f"feature_{i}" for i in range(len(X_test_l))])
+
+print(X_train.shape, X_valid.shape, X_test.shape)
+
+# Handle inf and -inf values
+X_train.replace([np.inf, -np.inf], np.nan, inplace=True)
+X_valid.replace([np.inf, -np.inf], np.nan, inplace=True)
+X_test.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(strategy="mean")
+
+X_train = pd.DataFrame(imputer.fit_transform(X_train), columns=X_train.columns)
+X_valid = pd.DataFrame(imputer.transform(X_valid), columns=X_valid.columns)
+X_test = pd.DataFrame(imputer.transform(X_test), columns=X_test.columns)
+
+# Remove duplicate columns
+X_train = X_train.loc[:, ~X_train.columns.duplicated()]
+X_valid = X_valid.loc[:, ~X_valid.columns.duplicated()]
+X_test = X_test.loc[:, ~X_test.columns.duplicated()]
+
+# 3) Train the model
 model_l = []  # list[tuple[model, predict_func,]]
 for f in DIRNAME.glob("model/model*.py"):
+    select_python_path = f.with_name(f.stem.replace("model", "select") + f.suffix)
+    select_m = import_module_from_path(select_python_path.stem, select_python_path)
+    X_train_selected = select_m.select(X_train.copy())
+    X_valid_selected = select_m.select(X_valid.copy())
+
     m = import_module_from_path(f.stem, f)
-    model_l.append((m.fit(X_train, y_train, X_valid, y_valid), m.predict))
+    model_l.append((m.fit(X_train_selected, y_train, X_valid_selected, y_valid), m.predict, select_m))
 
-print("X_valid", X_valid.head())
 
-# 3) Evaluate the model on the validation set
+# 4) Evaluate the model on the validation set
 metrics_all = []
-for model, predict_func in model_l:
-    y_valid_pred = predict_func(model, X_valid)
-
-    # Debug: Check for negative values in actual and predicted ConfirmedCases
-    print("Minimum value in y_valid['ConfirmedCases']:", y_valid["ConfirmedCases"].min())
-    print("Minimum value in y_valid_pred['ConfirmedCases']:", y_valid_pred["ConfirmedCases"].min())
-
-    # Debug: Print rows where actual or predicted values are negative
-    negative_actual = y_valid[y_valid["ConfirmedCases"] < 0]
-    negative_pred = y_valid_pred[y_valid_pred["ConfirmedCases"] < 0]
-
-    if not negative_actual.empty:
-        print("Rows with negative actual ConfirmedCases:")
-        print(negative_actual)
-
-    if not negative_pred.empty:
-        print("Rows with negative predicted ConfirmedCases:")
-        print(negative_pred)
+for model, predict_func, select_m in model_l:
+    X_valid_selected = select_m.select(X_valid.copy())
+    y_valid_pred = predict_func(model, X_valid_selected)
 
     # Add a small positive value to avoid negative or zero values
     epsilon = 1e-8
@@ -71,14 +100,15 @@ for model, predict_func in model_l:
     print(f"Average RMSLE on valid set: {rmsle_avg}")
     metrics_all.append(rmsle_avg)
 
-# 4) Save the validation accuracy
+# 5) Save the validation accuracy
 min_index = np.argmin(metrics_all)
 pd.Series(data=[metrics_all[min_index]], index=["RMSLE"]).to_csv("submission_score.csv")
 
-# 5) Make predictions on the test set and save them
-y_test_pred = model_l[min_index][1](model_l[min_index][0], X_test)
+# 6) Make predictions on the test set and save them
+X_test_selected = model_l[min_index][2].select(X_test.copy())
+y_test_pred = model_l[min_index][1](model_l[min_index][0], X_test_selected)
 
-# 6) Submit predictions for the test set
+# 7) Submit predictions for the test set
 submission_result = pd.DataFrame(
     {
         "ForecastId": forecast_ids,
