73b043fad41683bc721ab9e341d254f611fa5b46
Bowen Xian <xianbowen@outlook.com>
2024-10-30 05:11:23 +0000

    CI

--- rdagent/utils/env.py
+++ rdagent/utils/env.py
@@ -122,9 +122,9 @@ class LocalEnv(Env[LocalConf]):
 
 class DockerConf(BaseSettings):
     build_from_dockerfile: bool = False
-    dockerfile_folder_path: Optional[
-        Path
-    ] = None  # the path to the dockerfile optional path provided when build_from_dockerfile is False
+    dockerfile_folder_path: Optional[Path] = (
+        None  # the path to the dockerfile optional path provided when build_from_dockerfile is False
+    )
     image: str  # the image you want to build
     mount_path: str  # the path in the docker image to mount the folder
     default_entry: str  # the entry point of the image
