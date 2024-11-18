bf3e748e02ed2f4b5422859d13623de93c13c468
Linlang <Lv.Linlang@hotmail.com>
2024-11-18 15:30:18 +0800

    reformat with black

--- rdagent/app/utils/health_check.py
+++ rdagent/app/utils/health_check.py
@@ -7,14 +7,10 @@ from rdagent.log import rdagent_logger as logger
 def check_command_exists(command: str) -> bool:
     return shutil.which(command) is not None
 
+
 def check_command_execution(command: str) -> bool:
     command = command.split(" ")
-    result = subprocess.run(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True
-    )
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     if "Hello from Docker!" in result.stdout:
         return True
@@ -22,6 +18,7 @@ def check_command_execution(command: str) -> bool:
         print("STDOUT:", result.stdout)
         print("STDERR:", result.stderr)
         return False
+
 
 def check_docker():
     if check_command_exists("docker"):
@@ -31,9 +28,14 @@ def check_docker():
             if check_command_execution("sudo docker run hello-world"):
                 logger.warning(f"Please add the user to the docker user group.")
             else:
-                logger.error(f"Docker status is exception, please check the docker configuration or reinstall it. Refs: https://docs.docker.com/engine/install/ubuntu/.")
+                logger.error(
+                    f"Docker status is exception, please check the docker configuration or reinstall it. Refs: https://docs.docker.com/engine/install/ubuntu/."
+                )
     else:
-        logger.warning(f"Docker is not installed, please install docker. Refs: https://docs.docker.com/engine/install/ubuntu/.")
+        logger.warning(
+            f"Docker is not installed, please install docker. Refs: https://docs.docker.com/engine/install/ubuntu/."
+        )
+
 
 def health_check():
     check_docker()
