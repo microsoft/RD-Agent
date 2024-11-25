ae375ae773b2096ffd82dd883a281886dfa15d10
Xu Yang <xuyang1@microsoft.com>
2024-11-25 04:29:29 +0000

    remove redundant code

--- rdagent/components/coder/model_coder/evolving_strategy.py
+++ rdagent/components/coder/model_coder/evolving_strategy.py
@@ -30,71 +30,66 @@ class ModelMultiProcessEvolvingStrategy(MultiProcessEvolvingStrategy):
     ) -> str:
         model_information_str = target_task.get_task_information()
 
-        if queried_knowledge is not None and model_information_str in queried_knowledge.success_task_to_knowledge_dict:
-            return queried_knowledge.success_task_to_knowledge_dict[model_information_str].implementation
-        elif queried_knowledge is not None and model_information_str in queried_knowledge.failed_task_info_set:
-            return None
-        else:
-            queried_similar_successful_knowledge = (
-                queried_knowledge.task_to_similar_task_successful_knowledge[model_information_str]
-                if queried_knowledge is not None
-                else []
-            )
-            queried_former_failed_knowledge = (
-                queried_knowledge.task_to_former_failed_traces[model_information_str]
-                if queried_knowledge is not None
-                else []
-            )
+        queried_similar_successful_knowledge = (
+            queried_knowledge.task_to_similar_task_successful_knowledge[model_information_str]
+            if queried_knowledge is not None
+            else []
+        )
+        queried_former_failed_knowledge = (
+            queried_knowledge.task_to_former_failed_traces[model_information_str]
+            if queried_knowledge is not None
+            else []
+        )
 
-            queried_former_failed_knowledge_to_render = queried_former_failed_knowledge
+        queried_former_failed_knowledge_to_render = queried_former_failed_knowledge
 
-            system_prompt = (
+        system_prompt = (
+            Environment(undefined=StrictUndefined)
+            .from_string(
+                coder_prompts["evolving_strategy_model_coder"]["system"],
+            )
+            .render(
+                scenario=self.scen.get_scenario_all_desc(filtered_tag=target_task.model_type),
+                queried_former_failed_knowledge=queried_former_failed_knowledge_to_render,
+                current_code=target_task.base_code,
+            )
+        )
+
+        queried_similar_successful_knowledge_to_render = queried_similar_successful_knowledge
+        for _ in range(10):  # max attempt to reduce the length of user_prompt
+            user_prompt = (
                 Environment(undefined=StrictUndefined)
                 .from_string(
-                    coder_prompts["evolving_strategy_model_coder"]["system"],
+                    coder_prompts["evolving_strategy_model_coder"]["user"],
                 )
                 .render(
-                    scenario=self.scen.get_scenario_all_desc(filtered_tag=target_task.model_type),
+                    model_information_str=model_information_str,
+                    queried_similar_successful_knowledge=queried_similar_successful_knowledge_to_render,
                     queried_former_failed_knowledge=queried_former_failed_knowledge_to_render,
-                    current_code=target_task.base_code,
                 )
+                .strip("\n")
             )
-
-            queried_similar_successful_knowledge_to_render = queried_similar_successful_knowledge
-            for _ in range(10):  # max attempt to reduce the length of user_prompt
-                user_prompt = (
-                    Environment(undefined=StrictUndefined)
-                    .from_string(
-                        coder_prompts["evolving_strategy_model_coder"]["user"],
-                    )
-                    .render(
-                        model_information_str=model_information_str,
-                        queried_similar_successful_knowledge=queried_similar_successful_knowledge_to_render,
-                        queried_former_failed_knowledge=queried_former_failed_knowledge_to_render,
-                    )
-                    .strip("\n")
-                )
-                if (
-                    APIBackend().build_messages_and_calculate_token(
-                        user_prompt=user_prompt,
-                        system_prompt=system_prompt,
-                    )
-                    < LLM_SETTINGS.chat_token_limit
-                ):
-                    break
-                elif len(queried_former_failed_knowledge_to_render) > 1:
-                    queried_former_failed_knowledge_to_render = queried_former_failed_knowledge_to_render[1:]
-                elif len(queried_similar_successful_knowledge_to_render) > 1:
-                    queried_similar_successful_knowledge_to_render = queried_similar_successful_knowledge_to_render[1:]
-
-            code = json.loads(
-                APIBackend(use_chat_cache=CoSTEER_SETTINGS.coder_use_cache).build_messages_and_create_chat_completion(
+            if (
+                APIBackend().build_messages_and_calculate_token(
                     user_prompt=user_prompt,
                     system_prompt=system_prompt,
-                    json_mode=True,
-                ),
-            )["code"]
-            return code
+                )
+                < LLM_SETTINGS.chat_token_limit
+            ):
+                break
+            elif len(queried_former_failed_knowledge_to_render) > 1:
+                queried_former_failed_knowledge_to_render = queried_former_failed_knowledge_to_render[1:]
+            elif len(queried_similar_successful_knowledge_to_render) > 1:
+                queried_similar_successful_knowledge_to_render = queried_similar_successful_knowledge_to_render[1:]
+
+        code = json.loads(
+            APIBackend(use_chat_cache=CoSTEER_SETTINGS.coder_use_cache).build_messages_and_create_chat_completion(
+                user_prompt=user_prompt,
+                system_prompt=system_prompt,
+                json_mode=True,
+            ),
+        )["code"]
+        return code
 
     def assign_code_list_to_evo(self, code_list, evo):
         for index in range(len(evo.sub_tasks)):
