1b051e34677c547b7a672d065a776bbaa6ebb4b8
Bowen Xian <xianbowen@outlook.com>
2024-12-13 07:56:07 +0000

    fix data bug

--- rdagent/components/coder/data_science/raw_data_loader/eval.py
+++ rdagent/components/coder/data_science/raw_data_loader/eval.py
@@ -52,7 +52,7 @@ class DataLoaderCoSTEEREvaluator(CoSTEEREvaluator):
             )
 
         ds_docker_conf = DSDockerConf()
-        ds_docker_conf.extra_volumes = {f"{DS_RD_SETTING.local_data_path}/{DS_RD_SETTING.competition}": "/kaggle/input"}
+        ds_docker_conf.extra_volumes = {f"{DS_RD_SETTING.local_data_path}/{self.scen.competition}": "/kaggle/input"}
         de = DockerEnv(conf=ds_docker_conf)
 
         # TODO: do we need to clean the generated tempory content?
