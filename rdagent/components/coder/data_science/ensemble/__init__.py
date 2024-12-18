7f70ce2824265b9e871620a503e7f9048cdd8764
Xisen Wang <118058822+xisen-w@users.noreply.github.com>
2024-12-18 19:53:05 +0800

    Adding ensemble (#505)
    * Initial Draft
    
    * Updating logic for init
    
    * Revising
    
    * Successful Testing
    
    * Updating to use the latest & right class
    
    * bug: bug-fixing for testing

--- rdagent/components/coder/data_science/ensemble/__init__.py
+++ rdagent/components/coder/data_science/ensemble/__init__.py
@@ -1,19 +1,100 @@
-# from rdagent.components.coder.CoSTEER import CoSTEER
-# from rdagent.components.coder.CoSTEER.config import CoSTEER_SETTINGS
-# from rdagent.components.coder.CoSTEER.evaluators import CoSTEERMultiEvaluator
-# from rdagent.core.scenario import Scenario
+"""
+File structure
+- ___init__.py: the entrance/agent of coder
+- evaluator.py
+- conf.py
+- exp.py: everything under the experiment, e.g.
+    - Task
+    - Experiment
+    - Workspace
+- test.py
+    - Each coder could be tested.
+"""
+
+import json
+
+from rdagent.components.coder.CoSTEER import CoSTEER
+from rdagent.components.coder.CoSTEER.config import CoSTEER_SETTINGS
+from rdagent.components.coder.CoSTEER.evaluators import CoSTEERMultiEvaluator
+from rdagent.components.coder.CoSTEER.evolving_strategy import (
+    MultiProcessEvolvingStrategy,
+)
+from rdagent.components.coder.CoSTEER.knowledge_management import (
+    CoSTEERQueriedKnowledge,
+)
+from rdagent.components.coder.data_science.ensemble.eval import (
+    EnsembleCoSTEEREvaluator,
+)
+from rdagent.components.coder.data_science.ensemble.exp import EnsembleTask
+from rdagent.core.scenario import Scenario
+from rdagent.oai.llm_utils import APIBackend
+from rdagent.utils.agent.tpl import T
+
+from rdagent.core.experiment import FBWorkspace
 
 
-# class ModelEnsembleCoSTEER(CoSTEER):
-#     def __init__(
-#         self,
-#         scen: Scenario,
-#         *args,
-#         **kwargs,
-#     ) -> None:
-#         eva = CoSTEERMultiEvaluator(
-#             ModelEnsembleCoSTEEREvaluator(scen=scen), scen=scen
-#         )  # Please specify whether you agree running your eva in parallel or not
-#         es = ModelEnsembleMultiProcessEvolvingStrategy(scen=scen, settings=CoSTEER_SETTINGS)
+class EnsembleMultiProcessEvolvingStrategy(MultiProcessEvolvingStrategy):
+    def implement_one_task(
+        self,
+        target_task: EnsembleTask,
+        queried_knowledge: CoSTEERQueriedKnowledge | None = None,
+        workspace: FBWorkspace | None = None,
+    ) -> dict[str, str]:
+        # return a workspace with "ensemble.py" inside
+        competition_info = self.scen.get_scenario_all_desc()
+        # Generate code
+        system_prompt = T(".prompts:ensemble_coder.system").r(competition_info=competition_info)
+        user_prompt = T(".prompts:ensemble_coder.user").r(ensemble_spec=workspace.code_dict["spec/ensemble.md"])
 
-#         super().__init__(*args, settings=CoSTEER_SETTINGS, eva=eva, es=es, evolving_version=1, scen=scen, **kwargs)
+        ensemble_code = json.loads(
+            APIBackend().build_messages_and_create_chat_completion(
+                user_prompt=user_prompt,
+                system_prompt=system_prompt,
+                json_mode=True
+            )
+        )["code"]
+
+        return {
+            "ensemble.py": ensemble_code,
+        }
+    
+    def assign_code_list_to_evo(self, code_list: list[dict[str, str]], evo):
+        """
+        Assign the code list to the evolving item.
+
+        The code list is aligned with the evolving item's sub-tasks.
+        If a task is not implemented, put a None in the list.
+        """
+        for index in range(len(evo.sub_tasks)):
+            if code_list[index] is None:
+                continue
+            if evo.sub_workspace_list[index] is None:
+                # evo.sub_workspace_list[index] = FBWorkspace(target_task=evo.sub_tasks[index])
+                evo.sub_workspace_list[index] = evo.experiment_workspace
+            evo.sub_workspace_list[index].inject_code(**code_list[index])
+        return evo
+
+
+class EnsembleCoSTEER(CoSTEER):
+    def __init__(
+        self,
+        scen: Scenario,
+        *args,
+        **kwargs,
+    ) -> None:
+        eva = CoSTEERMultiEvaluator(
+            EnsembleCoSTEEREvaluator(scen=scen), scen=scen
+        )
+        es = EnsembleMultiProcessEvolvingStrategy(scen=scen, settings=CoSTEER_SETTINGS)
+
+        super().__init__(
+            *args,
+            settings=CoSTEER_SETTINGS,
+            eva=eva,
+            es=es,
+            evolving_version=2,
+            scen=scen,
+            **kwargs
+        )
+
+
