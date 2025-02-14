11cbc7d2fd6c316cd587ef6c49590beaf8430613
Xu Yang <peteryang@vip.qq.com>
2025-02-14 08:17:05 +0000

    small bugs in cache rerun

--- rdagent/utils/env.py
+++ rdagent/utils/env.py
@@ -11,6 +11,7 @@ import json
 import os
 import pickle
 import re
+import shutil
 import subprocess
 import time
 import uuid
@@ -416,6 +417,7 @@ class DockerEnv(Env[DockerConf]):
         """
         Unzip a file into a folder, use zipfile instead of subprocess
         """
+        shutil.rmtree(folder_path, ignore_errors=True)
         with zipfile.ZipFile(zip_file_path, "r") as z:
             z.extractall(folder_path)
 
