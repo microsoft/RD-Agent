c1952e0fccf8919da12e40016b9f9ccf327d4710
Xu <v-xuminrui@microsoft.com>
2025-02-21 11:24:05 +0000

    fix bugs on parallel processing of checkpoint

--- run_checkpoint.py
+++ run_checkpoint.py
@@ -7,33 +7,6 @@ import re
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from scripts.exp.researcher.utils import get_valid_submission
 
-def run_checkpoint_for_single_loop(log_path, loop, checkpoint):
-    command = [
-        "dotenv",
-        "run",
-        "--",
-        "env",
-        f"LOG_TRACE_PATH={log_path}",
-        "python",
-        "rdagent/app/data_science/loop.py",
-        "--path",
-        checkpoint,
-        "--loop_n",
-        "2",
-        ">",
-        f"{log_path}_{loop}.log",
-        "2>&1"
-    ]
-    try:
-        # Run the command and wait for it to finish
-        subprocess.run(" ".join(command), shell=True, check=True)
-        print(f"Successfully executed command for checkpoint: {checkpoint}")
-        return True
-    except subprocess.CalledProcessError as e:
-        print(f"Error while running command for checkpoint: {checkpoint}")
-        print(e)
-        return False
-
 def get_last_step(session_path):
     steps = os.listdir(session_path)
     idx, step = -1, ""
@@ -44,66 +17,72 @@ def get_last_step(session_path):
             step = s
     return step
 
-def process_single_competition(f, LOG_FOLDER, OUTPUT_FOLDER):
-    if f.is_dir() and "baseline" in f.name:
-        src = f"{LOG_FOLDER}/{f.name}"
-        dst = f"{OUTPUT_FOLDER}/{f.name.replace('baseline', 'researcher')}"
-        if os.path.exists(dst):
-            shutil.rmtree(dst)
-        shutil.copytree(src, dst)
 
-        output_path = f"{OUTPUT_FOLDER}/loop.json"
-        if os.path.exists(output_path):
-            with open(output_path, 'r') as file:
-                data = json.load(file)
-        else:
-            data = []
-        loop_info = {}
+def continue_checkpoint(loop_idx, src, dst, n_loops): 
+    if loop_idx != -1: 
+        path = f"{src}/__session__/{loop_idx}"
+        path = f"{path}/{get_last_step(path)}"
+        output_path = dst
+        command = [
+            "dotenv",
+            "run",
+            "--",
+            "python",
+            "rdagent/app/data_science/loop.py",
+            "--path",
+            path,
+            "--output_path",
+            output_path,
+            "--loop_n",
+            str(n_loops),
+            ">",
+            f"{output_path}_{loop_idx}.log",
+            "2>&1"
+        ]
+        try:
+            subprocess.run(" ".join(command), shell=True, check=True)
+            print(f"**Success** | Path: {path} | Output Path: {output_path}")
+            return True
+        except subprocess.CalledProcessError as e:
+            print(f"**Fail** | Path: {path} | Output Path: {output_path}")
+            print(e)
+            return False
+    return False
 
-        first_loop, last_loop = get_valid_submission(dst)
-        print(f"Competition: {f.name} | First Loop: {first_loop} | Last Loop: {last_loop}")
-        if first_loop != -1:
-            checkpoint = f"{dst}/__session__/{first_loop}"
-            checkpoint = f"{checkpoint}/{get_last_step(checkpoint)}"
-            status = run_checkpoint_for_single_loop(dst, first_loop, checkpoint)
-            if status:
-                loop_info['first_loop'] = first_loop
-        if last_loop != -1 and last_loop != first_loop:
-            checkpoint = f"{dst}/__session__/{last_loop}"
-            checkpoint = f"{checkpoint}/{get_last_step(checkpoint)}"
-            status = run_checkpoint_for_single_loop(dst, last_loop, checkpoint)
-            if status:
-                loop_info['last_loop'] = last_loop
 
-        data.append({f.name: loop_info})
+def run_single_checkpoint(competition, path, output_path, n_loops):
+    if competition.is_dir():
+        src = f"{path}/{competition.name}"
+        dst = f"{output_path}/{competition.name.replace('baseline', 'researcher')}"
 
-        with open(output_path, 'w') as file:
-            json.dump(data, file, indent=4)
+        first_loop, last_loop = get_valid_submission(src)
+        print(f"Competition: {competition.name} | First Loop: {first_loop} | Last Loop: {last_loop} | Output Path: {dst}")
+        continue_checkpoint(first_loop, src, dst, n_loops)
+        continue_checkpoint(last_loop, src, dst, n_loops)
 
 def main():
     args = arg_parser()
-    LOG_FOLDER = args.log_folder
-    OUTPUT_FOLDER = args.output_folder
-    num_process = args.num_process
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
 
-    if not os.path.exists(OUTPUT_FOLDER):
-        os.makedirs(OUTPUT_FOLDER)
-
-    with ThreadPoolExecutor(max_workers=num_process) as executor:
-        futures = [executor.submit(process_single_competition, f, LOG_FOLDER, OUTPUT_FOLDER) for f in os.scandir(LOG_FOLDER)]
+    with ThreadPoolExecutor(max_workers=args.n_process) as executor:
+        futures = [executor.submit(run_single_checkpoint, f, args.path, args.output_path, args.n_loops) for f in os.scandir(args.path)]
         for future in as_completed(futures):
             try:
                 future.result()
             except Exception as e:
-                print(f"Error processing folder: {e}")
+                print(f"**Fail** | {e}")
+
 
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--log_folder", type=str, default="./log1", help="The folder that store the log of baseline.")
-    parser.add_argument("--output_folder", type=str, default="./log2", help="The folder that store the log of researcher.")
-    parser.add_argument("--num_process", type=int, default=4, help="")
+    parser.add_argument("--path", type=str, default="./log_baseline", help="The checkpoint path that store the logs and sessions of baseline.")
+    parser.add_argument("--output_path", type=str, default="./log_researcher", help="The output path that save the logs and sessions.")
+    parser.add_argument("--n_process", type=int, default=4, help="Number of jobs to run in parallel.")
+    parser.add_argument("--n_loops", type=int, default=2, help="Number of loops to continue.")
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     main()
