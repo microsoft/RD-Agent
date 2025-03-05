428b74a988157ea864ebb40e828bd9f67589c863
you-n-g <you-n-g@users.noreply.github.com>
2025-03-05 22:27:46 +0800

    fix: direct_exp_gen to json_target_type in DSExpGen class (#661)
    * refactor: Rename direct_exp_gen to json_target_type in DSExpGen class
    
    * fix type

--- rdagent/scenarios/data_science/proposal/exp_gen.py
+++ rdagent/scenarios/data_science/proposal/exp_gen.py
@@ -420,7 +420,7 @@ class DSExpGen(ExpGen):
                             user_prompt=user_prompt,
                             system_prompt=system_prompt,
                             json_mode=True,
-                            direct_exp_gen=Dict[str, Dict[str, str]],
+                            json_target_type=dict[str, dict[str, str] | str],  # workflow_update may be a string
                         )
                     )
                     assert "hypothesis_proposal" in resp_dict, "Hypothesis proposal not provided."
