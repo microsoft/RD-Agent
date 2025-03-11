0e9b455b0fe37fe84dc51d41f6545765327e7be7
Young <afe.young@gmail.com>
2025-03-11 17:52:16 +0000

    fix: Correct directory navigation and dependency installation in entrypoint.sh

--- rdagent/scenarios/data_science/sing_docker/entrypoint.sh
+++ rdagent/scenarios/data_science/sing_docker/entrypoint.sh
@@ -7,12 +7,19 @@ sudo mkdir -p /mle/ /kaggle/
 sudo chown -R $USER:$USER /workspace/ /mle/ /kaggle/
 ls -lat /
 
-cd $DIR/../litellm-srv/
+cd $DIR/../RD-Agent
+mkdir -p log/
+git fetch
+git checkout ${RD_COMMIT:-ee8d97c52062607cac778b8aeb10769b075a8d11}
+make dev
+pip install 'litellm[proxy]'
+pip install git+https://github.com/you-n-g/litellm@add_mi_cred_pr
 
+
+cd $DIR/../litellm-srv/
 export AZURE_CLIENT_ID
 export AZURE_SCOPE=api://trapi/.default
 export AZURE_CREDENTIAL=ManagedIdentityCredential
-
 sed -i '/proxy_handler_instance/d' litellm.trapi.yaml  # remove useless handler in production
 nohup litellm --config litellm.trapi.yaml &
 
@@ -20,9 +27,6 @@ sleep 10  # wait for litellm to start
 
 
 cd $DIR/../RD-Agent
-mkdir -p log/
-git fetch
-git checkout ${RD_COMMIT:-ee8d97c52062607cac778b8aeb10769b075a8d11}
 script -c "timeout ${RD_TIMEOUT:-24h} python rdagent/app/data_science/loop.py --competition $DS_COMPETITION" log/stdout.${DS_COMPETITION}.log
 
 python rdagent/log/mle_summary.py grade_summary --log_folder=./log/
