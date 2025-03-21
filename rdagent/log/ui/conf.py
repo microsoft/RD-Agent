8bc107f7341887482211810ece9ea4ada693a5d0
XianBW <36835909+XianBW@users.noreply.github.com>
2025-03-21 18:11:06 +0800

    chore: update data science scenario UI (#717)
    * UI changes
    
    * remove hardcode
    
    * fix CI

---
+++ rdagent/log/ui/conf.py
@@ -0,0 +1,14 @@
+from pydantic_settings import SettingsConfigDict
+
+from rdagent.core.conf import ExtendedBaseSettings
+
+
+class UIBasePropSetting(ExtendedBaseSettings):
+    model_config = SettingsConfigDict(env_prefix="UI_", protected_namespaces=())
+
+    default_log_folders: list[str] = ["./log"]
+
+    baseline_result_path: str = "./baseline.csv"
+
+
+UI_SETTING = UIBasePropSetting()
