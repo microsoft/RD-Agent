617f6fda11ac7dfaa7a3a7fce5a77c8f4b0ca33c
xuangu-fang <xuangufang@gmail.com>
2025-04-17 01:36:43 +0000

    fix bug

--- rdagent/scenarios/data_science/dev/feedback.py
+++ rdagent/scenarios/data_science/dev/feedback.py
@@ -34,7 +34,7 @@ class DSExperiment2Feedback(Experiment2Feedback):
 
         # Get feedback description using shared template
         feedback_desc = T("scenarios.data_science.share:describe.feedback").r(
-            exp_and_feedback=last_exp, heading="Previous Trial Feedback"
+            exp_and_feedback=trace.hist[-1] if trace.hist else None, heading="Previous Trial Feedback"
         )
 
         # TODO:
