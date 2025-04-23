7b5a42121ea733cf85e5ec69612b42aa8dc53ed9
Xu Yang <peteryang@vip.qq.com>
2025-04-23 10:31:38 +0000

    update code

--- scripts/merge_ideas.py
+++ scripts/merge_ideas.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 def merge_jsons():
     merged_dict = {}
-    for idea_file in Path(r"/data/userdata/v-xuminrui/Knowledge/archive").iterdir():
+    for idea_file in Path(r"/data/userdata/v-xuminrui/Knowledge3").iterdir():
         competition_name = idea_file.stem
         merged_dict[competition_name] = json.load(idea_file.open())
     json.dump(
         merged_dict,
-        open(r"/data/userdata/v-xuminrui/Knowledge/archive/merged_ideas.json", "w"),
+        open(r"/data/userdata/v-xuminrui/Knowledge3/merged_ideas.json", "w"),
         indent=2,
     )
 
