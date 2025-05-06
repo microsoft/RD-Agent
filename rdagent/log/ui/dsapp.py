684c03a8d94cac21d2e595000daf3e7c890c7a67
XianBW <36835909+XianBW@users.noreply.github.com>
2025-05-06 11:34:21 +0800

    chore: ui change (#844)
    * ui change
    
    * ui change

--- rdagent/log/ui/dsapp.py
+++ rdagent/log/ui/dsapp.py
@@ -14,20 +14,29 @@ if "log_folders" not in state:
 
 summary_page = st.Page("ds_summary.py", title="Summary", icon="📊")
 trace_page = st.Page("ds_trace.py", title="Trace", icon="📈")
+aide_page = st.Page("aide.py", title="Aide", icon="🧑‍🏫")
 st.set_page_config(layout="wide", page_title="RD-Agent", page_icon="🎓", initial_sidebar_state="expanded")
-st.navigation([summary_page, trace_page]).run()
+st.navigation([summary_page, trace_page, aide_page]).run()
+
+
+def convert_log_folder_str(lf: str) -> str:
+    if "/" not in lf:
+        return f"/data/share_folder_local/amlt/{lf.strip()}/combined_logs"
+    return lf.strip()
+
 
 # UI - Sidebar
 with st.sidebar:
     st.subheader("Pages", divider="rainbow")
     st.page_link(summary_page, icon="📊")
     st.page_link(trace_page, icon="📈")
+    st.page_link(aide_page, icon="🧑‍🏫")
 
     st.subheader("Settings", divider="rainbow")
     with st.form("log_folder_form", border=False):
-        log_folder_str = st.text_area(
-            "**Log Folders**(split by ';')", placeholder=state.log_folder, value=";".join(state.log_folders)
-        )
+        log_folder_str = st.text_area("**Log Folders**(split by ';')", placeholder=state.log_folder)
         if st.form_submit_button("Confirm"):
-            state.log_folders = [folder.strip() for folder in log_folder_str.split(";") if folder.strip()]
+            state.log_folders = [
+                convert_log_folder_str(folder) for folder in log_folder_str.split(";") if folder.strip()
+            ]
             st.rerun()
