871686214ad7415ba1660e64fde90c2e504a2136
xuangu-fang <xuangufang@gmail.com>
2025-05-08 07:13:11 +0000

    auto lint

--- rdagent/scenarios/data_science/proposal/exp_gen/proposal.py
+++ rdagent/scenarios/data_science/proposal/exp_gen/proposal.py
@@ -242,12 +242,9 @@ class DSProposalV2ExpGen(ExpGen):
         )
         return json.loads(response)
 
-
-    def identify_feedback_problem(self,
-                                  scenario_desc: str,
-                                  exp_feedback_list_desc: str,
-                                  sota_exp_desc: str,
-                                  inject_diverse: bool = False) -> Dict:
+    def identify_feedback_problem(
+        self, scenario_desc: str, exp_feedback_list_desc: str, sota_exp_desc: str, inject_diverse: bool = False
+    ) -> Dict:
         sys_prompt = T(".prompts_v2:feedback_problem.system").r(
             problem_spec=T(".prompts_v2:specification.problem").r(),
             problem_output_format=T(".prompts_v2:output_format.problem").r(),
@@ -504,7 +501,7 @@ class DSProposalV2ExpGen(ExpGen):
                 exp_feedback_list_desc=exp_feedback_list_desc,
                 sota_exp_desc=sota_exp_desc,
                 inject_diverse=inject_diverse,
-        )
+            )
             for problem_name in fb_problems:
                 fb_problems[problem_name]["label"] = "FEEDBACK_PROBLEM"
                 all_problems[problem_name] = fb_problems[problem_name]
