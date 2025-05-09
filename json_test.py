c859612e4cfd008e1485abdcd99ab3f98376a985
Yuge Zhang <scottyugochang@gmail.com>
2025-05-09 17:39:17 +0800

    minor updates

--- json_test.py
+++ json_test.py
@@ -4,26 +4,26 @@ from pydantic import BaseModel
 import dotenv
 
 dotenv.load_dotenv(override=True)
-# messages = [{"role": "user", "content": "List 5 important events in the XIX century"}]
+messages = [{"role": "user", "content": "List 5 important events in the XIX century"}]
 
-# class CalendarEvent(BaseModel):
-#   name: str
-#   date: str
-#   participants: list[str]
+class CalendarEvent(BaseModel):
+  name: str
+  date: str
+  participants: list[str]
 
-# class EventsList(BaseModel):
-#     events: list[CalendarEvent]
+class EventsList(BaseModel):
+    events: list[CalendarEvent]
 
-# resp = completion(
-#     model=os.environ["CHAT_MODEL"],
-#     messages=messages,
-#     response_format=EventsList
-# )
+resp = completion(
+    model='azure/o3-mini',
+    messages=messages,
+    # response_format=EventsList
+)
 
-# print("Received={}".format(resp))
+print("Received={}".format(resp))
 
 from litellm import get_supported_openai_params
 
-params = get_supported_openai_params(model=os.environ["CHAT_MODEL"])
+params = get_supported_openai_params(model='azure/o3-mini')
 
 assert "response_format" in params
