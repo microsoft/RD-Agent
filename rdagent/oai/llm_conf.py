ef88ef69a6ff7a764f73220e49e9d0e3c4bcf948
Young <afe.young@gmail.com>
2025-05-29 06:52:26 +0000

    docs: clarify purpose of reasoning_think_rm in LLMSettings

--- rdagent/oai/llm_conf.py
+++ rdagent/oai/llm_conf.py
@@ -19,6 +19,10 @@ class LLMSettings(ExtendedBaseSettings):
 
     # Handling format
     reasoning_think_rm: bool = False
+    """
+    Some LLMs include <think>...</think> tags in their responses, which can interfere with the main output.
+    Set reasoning_think_rm to True to remove any <think>...</think> content from responses.
+    """
 
     # TODO: most of the settings are only used on deprec.DeprecBackend.
     # So they should move the settings to that folder.
