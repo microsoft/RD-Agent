01c876ee5bfaf8636f5b5b89078810b31ed95388
Bowen Xian <xianbowen@outlook.com>
2025-06-05 03:36:38 +0000

    change qlib install method

--- rdagent/utils/env.py
+++ rdagent/utils/env.py
@@ -537,11 +537,7 @@ class QlibCondaEnv(LocalEnv[QlibCondaConf]):
                     shell=True,
                 )
                 subprocess.check_call(
-                    f"conda run -n {self.conf.conda_env_name} rm -rf qlib; git clone https://github.com/microsoft/qlib.git",
-                    shell=True,
-                )
-                subprocess.check_call(
-                    f"conda run -n {self.conf.conda_env_name} bash -c 'cd qlib && git reset --hard 3e72593b8c985f01979bebcf646658002ac43b00 && make dev .'",
+                    f"conda run -n {self.conf.conda_env_name} pip install git+https://github.com/microsoft/qlib.git@3e72593b8c985f01979bebcf646658002ac43b00",
                     shell=True,
                 )
                 subprocess.check_call(
