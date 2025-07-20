5accec37c8828ac42005c2d12b815bef599b547e
you-n-g <you-n-g@users.noreply.github.com>
2025-07-20 16:31:43 +0800

    feat: add extra_eval config and import_class for custom evaluators (#1097)
    * feat: add extra_eval config and import_class for custom evaluators
    
    * lint
    
    * build: update litellm requirement to >=1.73 for get_valid_models
    
    * refactor: remove *args/**kwargs from _create_embedding_inner_function signature

--- rdagent/components/coder/data_science/conf.py
+++ rdagent/components/coder/data_science/conf.py
@@ -23,6 +23,18 @@ class DSCoderCoSTEERSettings(CoSTEERSettings):
     env_type: str = "docker"
     # TODO: extract a function for env and conf.
 
+    extra_eval: list[str] = []
+    """
+    Extra evaluators
+
+    The evaluator follows the following assumptions:
+    - It runs after previous evaluator (So the running results are alreadly there)
+
+    It is not a complete feature due to it is only implemented in DS Pipeline & Coder.
+
+    TODO: The complete version should be implemented in the CoSTEERSettings.
+    """
+
 
 def get_ds_env(
     conf_type: Literal["kaggle", "mlebench"] = "kaggle",
