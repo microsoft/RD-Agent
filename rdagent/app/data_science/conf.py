7834ff2b8f9fd6aa28fb9b651f2c73ad3c37ce9a
Xu Yang <peteryang@vip.qq.com>
2025-08-04 09:30:13 +0000

    a little refinement

--- rdagent/app/data_science/conf.py
+++ rdagent/app/data_science/conf.py
@@ -132,6 +132,9 @@ class DataScienceBasePropSetting(KaggleBasePropSetting):
     enable_hypo_critique_rewrite: bool = True
     """Enable hypothesis critique and rewrite stages for improving hypothesis quality"""
     enable_scale_check: bool = False
+    
+    #### enable runner code change summary
+    runner_enable_code_change_summary: bool = True
 
 
 DS_RD_SETTING = DataScienceBasePropSetting()
