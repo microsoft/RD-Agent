4470b2c89ca5692056fc46f0c47bae86a98b0023
qew21 <illking@foxmail.com>
2025-08-04 08:24:55 +0000

    update tpl

--- rdagent/utils/agent/tpl.py
+++ rdagent/utils/agent/tpl.py
@@ -15,7 +15,7 @@ from rdagent.core.conf import RD_AGENT_SETTINGS
 from rdagent.log import rdagent_logger as logger
 
 DIRNAME = Path(__file__).absolute().resolve().parent
-PROJ_PATH = DIRNAME.parent.parent
+PROJ_PATH = DIRNAME.parent.parent  # rdagent
 
 
 def get_caller_dir(upshift: int = 0) -> Path:
@@ -45,7 +45,7 @@ def load_content(uri: str, caller_dir: Path | None = None, ftype: str = "yaml") 
     if path_part.startswith("."):
         file_path_l = [caller_dir / f"{path_part[1:].replace('.', '/')}.{ftype}"]
         if RD_AGENT_SETTINGS.app_tpl is not None:
-            file_path_l.insert(0, Path("rdagent") / RD_AGENT_SETTINGS.app_tpl / file_path_l[0].relative_to(PROJ_PATH))
+            file_path_l.insert(0, PROJ_PATH / RD_AGENT_SETTINGS.app_tpl / file_path_l[0].relative_to(PROJ_PATH))
     else:
         file_path_l = [
             Path(path_part.replace(".", "/")).with_suffix(f".{ftype}"),
@@ -54,9 +54,9 @@ def load_content(uri: str, caller_dir: Path | None = None, ftype: str = "yaml") 
         # NOTE: for application's template to override the default template
         if RD_AGENT_SETTINGS.app_tpl is not None:
             file_path_l.insert(
-                0, (Path("rdagent") / RD_AGENT_SETTINGS.app_tpl / path_part.replace(".", "/")).with_suffix(f".{ftype}")
+                0, (PROJ_PATH / RD_AGENT_SETTINGS.app_tpl / path_part.replace(".", "/")).with_suffix(f".{ftype}")
             )
-            file_path_l.insert(0, (Path("rdagent").parent / path_part.replace(".", "/")).with_suffix(f".{ftype}"))
+            file_path_l.insert(0, (PROJ_PATH.parent / path_part.replace(".", "/")).with_suffix(f".{ftype}"))
 
     for file_path in file_path_l:
         try:
