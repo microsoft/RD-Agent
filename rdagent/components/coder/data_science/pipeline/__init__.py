d2f6855be5776f6660a6d5ace6cb096ae9b78f27
Paul Wang <paul_wang347@hotmail.com>
2025-08-04 17:49:24 -0700

    fix for merge

--- rdagent/components/coder/data_science/pipeline/__init__.py
+++ rdagent/components/coder/data_science/pipeline/__init__.py
@@ -83,7 +83,6 @@ class PipelineMultiProcessEvolvingStrategy(MultiProcessEvolvingStrategy):
             package_info=target_task.package_info,
             enable_model_dump=DS_RD_SETTING.enable_model_dump,
             enable_debug_mode=DS_RD_SETTING.sample_data_by_LLM,
-            enable_notebook_conversion=DS_RD_SETTING.enable_notebook_conversion,
             spec=T("scenarios.data_science.share:component_spec.Pipeline").r(
                 metric_name=self.scen.metric_name,
                 enable_notebook_conversion=DS_RD_SETTING.enable_notebook_conversion,
