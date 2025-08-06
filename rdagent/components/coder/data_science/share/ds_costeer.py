f787ed216958463dbb794c83bb67e4af9757a11b
Xu Yang <peteryang@vip.qq.com>
2025-08-06 13:29:53 +0000

    fix a small bug, only consider score in runner

--- rdagent/components/coder/data_science/share/ds_costeer.py
+++ rdagent/components/coder/data_science/share/ds_costeer.py
@@ -1,5 +1,4 @@
 from rdagent.components.coder.CoSTEER import CoSTEER
-from rdagent.components.coder.CoSTEER.evaluators import CoSTEERMultiFeedback
 
 
 class DSCoSTEER(CoSTEER):
@@ -8,26 +7,3 @@ class DSCoSTEER(CoSTEER):
         The coder uses the scenario's real debug timeout as the maximum seconds for development.
         """
         return int(self.scen.real_debug_timeout() * self.settings.max_seconds_multiplier)
-
-    def compare_and_pick_fb(self, base_fb: CoSTEERMultiFeedback | None, new_fb: CoSTEERMultiFeedback | None) -> bool:
-        # In data science, we only have a single feedback.
-        # Note: new_fb should always exists as indicated by _get_last_fb() function.
-        if base_fb is None:
-            return True
-
-        base_fb = base_fb[0]
-        new_fb = new_fb[0]
-
-        def compare_scores(s1, s2) -> bool:
-            if s1 is None:
-                if s2 is None:  # FIXME: is this possible?
-                    return False
-                if s2 is not None:
-                    return True
-            else:
-                if s2 is None:  # new is invalid
-                    return False
-                else:
-                    return (s2 > s1) == self.scen.metric_direction
-
-        return compare_scores(base_fb.score, new_fb.score)
