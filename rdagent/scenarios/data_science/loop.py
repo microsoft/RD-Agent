f2e3486ca6954afd493d4d971b7341e4ea690e6a
Young <afe.young@gmail.com>
2025-08-29 21:25:56 +0000

    fix: allow prev_out keys to be None in workspace cleanup assertion

--- rdagent/scenarios/data_science/loop.py
+++ rdagent/scenarios/data_science/loop.py
@@ -312,7 +312,7 @@ class DataScienceRDLoop(RDLoop):
             # only clean current workspace without affecting other loops.
             for k in "direct_exp_gen", "coding", "running":
                 if k in prev_out:
-                    assert isinstance(prev_out[k], DSExperiment)
+                    assert prev_out[k] is None or isinstance(prev_out[k], DSExperiment)
                     clean_workspace(prev_out[k].experiment_workspace.workspace_path)
 
             # Backup the workspace (only necessary files are included)
