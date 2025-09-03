2ec6a143c6c3e5f962668845d074c66a40bca3f2
Chaos Yu <chaos.you@gmail.com>
2025-09-03 08:26:19 +0000

    add an argument to disable remaining number check

--- rdagent/app/data_science/loop.py
+++ rdagent/app/data_science/loop.py
@@ -12,8 +12,10 @@ from rdagent.log import rdagent_logger as logger
 from rdagent.scenarios.data_science.loop import DataScienceRDLoop
 
 
-async def run_and_auto_submit(loop_task: Coroutine, competition: str):
-    """Run the loop coroutine task, and submit the SOTA experiment file (.csv only now) to kaggle at the end."""
+async def run_and_auto_submit(loop_task: Coroutine, competition: str, ensure_remaining_num: bool=True):
+    """Run the loop coroutine task, and submit the SOTA experiment file (.csv only now) to kaggle at the end.
+    Disable ensure_remaining_num if you are trying to submit to a completed competition.
+    """
     from datetime import datetime
     from rdagent.log.conf import LOG_SETTINGS
     from rdagent.log.ui.utils import get_sota_exp_stat
@@ -49,17 +51,18 @@ async def run_and_auto_submit(loop_task: Coroutine, competition: str):
     logger.info(f"Current sota submission file: {submission_csv_path}")
 
     # check if we have remaining submit number
-    remaining_submit_num = get_rest_submit_num(competition=competition)
+    if ensure_remaining_num:
+        remaining_submit_num = get_rest_submit_num(competition=competition)
 
-    if remaining_submit_num == 0:
-        logger.warning(
-            (
-                "Cannot submit now, as no remaining number today.\n"
-                "NOTE: also please confirm that you have joined the competition, or the remaining number is 0."
+        if remaining_submit_num == 0:
+            logger.warning(
+                (
+                    "Cannot submit now, as no remaining number today.\n"
+                    "NOTE: also please confirm that you have joined the competition, and the competition was not completed."
+                )
             )
-        )
 
-        return
+            return
 
     # do submit
     submit_csv(competition=competition, submission_file=str(submission_csv_path), wait=True, msg=f"SOTA at {datetime.now()}")
