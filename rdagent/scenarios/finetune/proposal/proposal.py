5bb1bd8212da0a0d61cf05c80fb50e0827cf0dba
Qizheng Li <jenssenlee@163.com>
2025-10-20 05:39:56 +0000

    fix(proposal): yaml path bug

--- rdagent/scenarios/finetune/proposal/proposal.py
+++ rdagent/scenarios/finetune/proposal/proposal.py
@@ -178,8 +178,8 @@ class LLMFinetuneExpGen(ExpGen):
             # Use template system for prompts
             # TODO: dicide more
             # TODO: separate first loop and other loop(need to get previous feedback)
-            system_prompt = T(".prompts.hypothesis_gen.system_prompt").r(**template_context)
-            user_prompt = T(".prompts.hypothesis_gen.user_prompt").r(**template_context)
+            system_prompt = T(".prompts:hypothesis_gen.system_prompt").r(**template_context)
+            user_prompt = T(".prompts:hypothesis_gen.user_prompt").r(**template_context)
 
             # Call LLM for selection
             from rdagent.oai.llm_utils import APIBackend
