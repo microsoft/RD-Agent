ec13408d9931adc2c01bf7101b40c7b8c0c6ea7c
jingyuanlm <842442862@qq.com>
2026-01-07 10:11:50 +0000

    add ckpt 8

--- rdagent/app/data_science/conf.py
+++ rdagent/app/data_science/conf.py
@@ -201,7 +201,7 @@ class DataScienceBasePropSetting(KaggleBasePropSetting):
     enable_reward_model_selection: bool = True
     """Enable reward model based hypothesis selection."""
     
-    reward_model_path: str ="/data/Blob_EastUS/FinetuneAgenticLLM/reward_ckpt/last_run_7" #"/data/userdata/v-lijingyuan/last_run_2"# "/data/userdata/v-lijingyuan/last_run_2"#"/data/Blob_EastUS/FinetuneAgenticLLM/reward_ckpt/last_run_2"
+    reward_model_path: str ="/data/Blob_EastUS/FinetuneAgenticLLM/reward_ckpt/last_run_8" #"/data/userdata/v-lijingyuan/last_run_2"# "/data/userdata/v-lijingyuan/last_run_2"#"/data/Blob_EastUS/FinetuneAgenticLLM/reward_ckpt/last_run_2"
     
     #"/data/Blob_EastUS/FinetuneAgenticLLM/reward_ckpt/last_run_2"
     #"/data/userdata/v-lijingyuan/last_run_2"
